d998260bc1829d304953ab6ad3afcc9d80a4c288
Tommy Toban <tommy.deppatori@gmail.com>
2021-04-30 16:50:46 +0700

    edit notifikasi sukses payment

--- pages/notifikasi.js
+++ pages/notifikasi.js
@@ -1,12 +1,62 @@
-import Link from 'next/link'
+import React from 'react';
+import Container from '@material-ui/core/Container';
+import Box from '@material-ui/core/Box';
+import dynamic from 'next/dynamic';
+import Link from 'next/link';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import CardMedia from '@material-ui/core/CardMedia';
+
+const GenAppBar = dynamic(
+    () => import('./../components/general/GenAppBar'),
+    { ssr: false }
+)
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+      flexGrow: 1,
+    },
+    menuButton: {
+      marginRight: theme.spacing(2),
+    },
+    title: {
+      flexGrow: 1,
+    },
+  }));
 
 const Notifikasi = ()=>{
+    const classes = useStyles();
+
     return (
         <>
-            <h1>Yeah! Pembelian sudah berhasil di kirim ke penjual</h1>
-            <Link href="/home">
-                <div>Belanja lagi</div>
-            </Link>
+            <GenAppBar nama="Notifikasi" />
+            <Container>
+                <Box my={2}>
+                <CardMedia
+                    component="img"
+                    alt="Sukses"
+                    height="140"
+                    image="/others/paymentsukses.jpg"
+                    title="Sukses"
+                />
+                <Typography variant="h5" component="h2">
+                Sukses! Pembayaran sudah kami terima.
+                </Typography>
+                <Typography variant="h8" component="p" color="textSecondary">
+                Terima kasih untuk pembayarannya. Nota pembayaran akan dikirimkan ke email terdaftar Anda.
+                </Typography>
+         
+                <Link href="/home">
+                <Button color="primary">
+                 Belanja lagi
+                </Button>
+                   
+                </Link>
+            
+                </Box>
+            </Container>
+            
            
         </>
     )
